774b7b0f1feb0c94261ae05994a3648e6de1e925
Slaytanic <horseman@163.com>
2012-11-30 13:40:52 +0800

    add onlydb and reduce columns

--- phpHiveAdmin.sql
+++ phpHiveAdmin.sql
@@ -1,15 +1,17 @@
-CREATE TABLE IF NOT EXISTS `pha_user` (
+CREATE TABLE IF NOT EXISTS `ehm_pha_user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(60) NOT NULL,
   `password` varchar(100) NOT NULL,
+  `onlydb` varchar(200) NOT NULL,
   `role` varchar(50) NOT NULL,
+  `reduce` int(10) NOT NULL,
   `access_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `description` text NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-CREATE TABLE IF NOT EXISTS `pha_job` (
+CREATE TABLE IF NOT EXISTS `ehm_pha_job` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `username` varchar(60) NOT NULL,
   `fingerprint` varchar(100) NOT NULL,
