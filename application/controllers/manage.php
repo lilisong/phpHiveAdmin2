90faadf227f7c6f900619f7a9aa81f54b09a0ba0
xianglei <horseman@163.com>
2012-12-07 19:50:52 +0800

    debug

--- application/controllers/manage.php
+++ application/controllers/manage.php
@@ -10,7 +10,7 @@ class Manage extends CI_Controller
 	
 	public function login()
 	{
-		$this->lang->load('common_lang', 'chinese');
+		$this->lang->load('common', 'chinese');
 		
 		$data['lang'] = $this->lang->line('common_lang_set');
 		$data['title'] = $this->lang->line('common_title');
