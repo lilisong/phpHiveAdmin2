aac739bd7e0446d0d36087837113f74413ae5f14
xianglei <horseman@163.com>
2012-12-07 17:29:16 +0800

    lang learn

--- application/controllers/manage.php
+++ application/controllers/manage.php
@@ -1,15 +1,22 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Manage extends CI_Controller {
+class Manage extends CI_Controller
+{
 
 	public function index()
 	{
 		$this->load->view('index');
 	}
-    public function login()
-    {
-        $this->load->view('header');
-        $this->load->view('login');
-        $this->load->view('footer');
-    }
+	
+	public function login()
+	{
+		$this->lang->load('common_lang', 'chinese');
+		
+		$data['lang'] = $this->lang->line('common_lang_set');
+		$data['title'] = $this->lang->line('common_title');
+		
+		$this->load->view('header');
+		$this->load->view('login');
+		$this->load->view('footer');
+	}
 }
