3933350fa9baf6423abfb3ff4216820ca463dc38
xianglei <horseman@163.com>
2013-01-10 21:16:27 +0800

    debug

--- application/views/sql_query_status_modal.php
+++ application/views/sql_query_status_modal.php
@@ -4,21 +4,29 @@ function QueryStatus()
 {
 	$.post('<?php echo $this->config->base_url();?>index.php/manage/getquerystatus/', { finger_print:$('#finger_print').val() }, function(html){
 		html = html;
-		$('#sql_query_status').html(html);
+		$('#sql_query_status').html('<small>' + html + '</small>');
 	});
 }
 
 function RefreshStatus(ctrl)
 {
-		setInterval(QueryStatus,2000);
+	if(ctrl == true)
+	{
+		self.timer = setInterval(QueryStatus,2000);
+	}
+	else
+	{
+		clearInterval(self.timer);
+	}
 }
 
+//var int=self.setInterval("QueryStatus()",2000)
 
 </script>
 
 <div id="sql_query_status_modal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true" onclick="int=window.clearInterval(int)">&times;</button>
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true" onclick="RefreshStatus(false)">&times;</button>
 		<h3 id="myModalLabel"><?php echo $var_db_name;?></h3>
 	</div>
 	<div class="modal-body">
@@ -30,6 +38,6 @@ function RefreshStatus(ctrl)
 	</div>
 	<div class="modal-footer">
 		<a href="" class="btn"><?php echo $common_cli_done;?></a>
-		<a href="#" class="btn btn-primary" onclick="SqlQuery();RefreshStatus()"><?php echo $common_submit;?></a>
+		<a href="#" class="btn btn-primary" onclick="SqlQuery();RefreshStatus(true)"><?php echo $common_submit;?></a>
 	</div>
 </div>
