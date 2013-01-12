c1f13a8a7857c59ad4950603a54f8a9ea5c797c7
xianglei <horseman@163.com>
2013-01-13 02:23:19 +0800

    update:   add try catch action to hdfs_model.php

--- application/models/hdfs_model.php
+++ application/models/hdfs_model.php
@@ -25,27 +25,33 @@ class Hdfs_model extends CI_Model
 		$this->hive->async_execute_hql($cmd, $filename, 1, $code);
 		
 		$this->load->model('utilities_model', 'utils');
-		$list_arr = @file($filename);
-		$i = 0;
-		foreach($list_arr as $k => $line)
+		try
 		{
-			if(!preg_match("/Found/i", $line))
+			$list_arr = @file($filename);
+			$i = 0;
+			foreach($list_arr as $k => $line)
 			{
-				$cols = explode(" ", $line);
-				$cols = $this->utils->array_filter($cols);
-				$arr['file_property'][$i] = $cols[0];
-				$arr['file_user'][$i] = $cols[2];
-				$arr['file_group'][$i] = $cols[3];
-				$arr['file_size'][$i] = $cols[4];
-				$arr['file_time'][$i] = $cols[5] . " " . $cols[6];
-				$arr['file_name'][$i] = $cols[7];
+				if(!preg_match("/Found/i", $line))
+				{
+					$cols = explode(" ", $line);
+					$cols = $this->utils->array_filter($cols);
+					$arr['file_property'][$i] = $cols[0];
+					$arr['file_user'][$i] = $cols[2];
+					$arr['file_group'][$i] = $cols[3];
+					$arr['file_size'][$i] = $cols[4];
+					$arr['file_time'][$i] = $cols[5] . " " . $cols[6];
+					$arr['file_name'][$i] = $cols[7];
+				}
+				$i++;
 			}
-			$i++;
+		
+			unlink($filename);
+			return $arr;
 		}
-		
-		unlink($filename);
-		
-		return $arr;
+		catch (Exception $e)
+		{
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+		}
 	}
 
 }
