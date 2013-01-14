fa17c845d3bcabdaed03020f2f04bc3a3b93bb35
xianglei <horseman@163.com>
2013-01-14 18:27:01 +0800

    debug

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -11,7 +11,7 @@ class User extends CI_Controller
 			redirect($this->config->base_url() . 'index.php/user/login/');
 			if($this->session->userdata('role') != "admin")
 			{
-				redirect($this->config->base_url() . 'index.php/user/login/');
+				redirect($this->config->base_url() . 'index.php/manage/index/');
 			}
 		}
 	}
